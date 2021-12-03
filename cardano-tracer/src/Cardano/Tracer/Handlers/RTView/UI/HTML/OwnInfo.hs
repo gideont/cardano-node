7aaae3ab3ed9f2a4bf3e6dd51ab584ac48ebc67f
Denis Shevchenko <denis.shevchenko@iohk.io>
2021-12-03 19:59:46 +0400

    test me

--- cardano-tracer/src/Cardano/Tracer/Handlers/RTView/UI/HTML/OwnInfo.hs
+++ cardano-tracer/src/Cardano/Tracer/Handlers/RTView/UI/HTML/OwnInfo.hs
@@ -9,12 +9,14 @@ module Cardano.Tracer.Handlers.RTView.UI.HTML.OwnInfo
 import           Control.Monad (forM, void)
 import qualified Data.Text as T
 import           Data.Version (showVersion)
-import           System.FilePath.Posix (takeDirectory)
-
 import qualified Graphics.UI.Threepenny as UI
 import           Graphics.UI.Threepenny.Core (Element, UI, element, liftIO, set, string, text, (#), (#+), (#.))
+import           System.FilePath.Posix (takeDirectory)
+
+import           Cardano.Config.Git.Rev (gitRev)
 
 import           Cardano.Tracer.Handlers.RTView.UI.Img.Icons
+import           Cardano.Tracer.Handlers.RTView.UI.Utils
 
 {-
 import           Cardano.RTView.CLI (RTViewParams (..))
@@ -24,7 +26,6 @@ import           Cardano.RTView.Config (configFileIsProvided, notificationsFileI
 import           Cardano.RTView.GUI.Elements (HTMLClass (..), (#.), hideIt)
 import           Cardano.RTView.GUI.JS.Utils (copyTextToClipboard)
 import           Cardano.RTView.Git.Rev (gitRev)
-import           Cardano.RTView.SupportedNodes (supportedNodesVersions)
 import           Paths_cardano_rt_view (version)
 -}
 
@@ -37,32 +38,31 @@ mkOwnInfo closeIt =
             [ UI.div #. "box" #+
                 [ UI.div #. "columns" #+
                     [ UI.div #. "column has-text-right" #+
-                        [ UI.p #. "mb-1" #+ [ string "Version"
-                                            , image "rt-view-what-icon" questionSVG
-                                                    # set UI.title__ "Version of cardano-tracer RTView is a part of"
-                                            ]
-                        , UI.p #. "mb-1" #+ [ string "Commit"
-                                            , image "rt-view-what-icon" questionSVG
-                                                    # set UI.title__ "Git commit cardano-tracer was built from"
-                                            ]
-                        , UI.p #. "mb-1" #+ [ string "Platform"
-                                            , image "rt-view-what-icon" questionSVG
-                                                    # set UI.title__ "Platform cardano-tracer is running on"
-                                            ]
-                        , UI.p           #+ [ string "Supported nodes"
-                                            , image "rt-view-what-icon" questionSVG
-                                                    # set UI.title__ "Versions of the nodes RTView was tested with"
-                                            ]
+                        [ UI.p #. "mb-1" #+
+                            [ string "Version"
+                            , image "rt-view-what-icon" questionSVG
+                                    # set UI.title__ "Version of cardano-tracer RTView is a part of"
+                            ]
+                        , UI.p #. "mb-1" #+
+                            [ string "Commit"
+                            , image "rt-view-what-icon" questionSVG
+                                    # set UI.title__ "Git commit cardano-tracer was built from"
+                            ]
+                        , UI.p #. "mb-1" #+
+                            [ string "Platform"
+                            , image "rt-view-what-icon" questionSVG
+                                    # set UI.title__ "Platform cardano-tracer is running on"
+                            ]
+                        , UI.p #+
+                            [ string "Supported nodes"
+                            , image "rt-view-what-icon" questionSVG
+                                    # set UI.title__ "Versions of the nodes RTView was tested with"
+                            ]
                         ]
                     , UI.div #. "column has-text-weight-semibold" #+
                         [ UI.p #. "mb-1" #+ [string "1.0"]
-                        , UI.p #. "mb-1" #+ [string "abcdefg"]
+                        , UI.p #. "mb-1" #+ [string $ T.unpack gitRev]
                         , UI.p #. "mb-1" #+ [string "Linux"]
-                        , UI.div #. "tags are-medium" #+
-                            [ UI.span #. "tag is-link" # set text "1.27.0"
-                            , UI.span #. "tag is-link" # set text "1.28.0"
-                            , UI.span #. "tag is-link" # set text "1.29.0"
-                            ]
                         ]
                     ]
                 ]
@@ -70,5 +70,3 @@ mkOwnInfo closeIt =
         ]
     , element closeIt
     ]
- where
-  image imgClass svg = UI.span #. imgClass # set UI.html svg
